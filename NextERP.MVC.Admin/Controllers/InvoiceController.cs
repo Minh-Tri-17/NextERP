34a96a51dde459214b2e29ccb0342931b98089b9
Minh-Tri-17 <minhtri17082001@gmail.com>
2025-10-14 15:34:00 +0700

    Refactor: enhance js

--- NextERP.MVC.Admin/Controllers/InvoiceController.cs
+++ NextERP.MVC.Admin/Controllers/InvoiceController.cs
@@ -1,9 +1,14 @@
 ﻿using LazZiya.ExpressLocalization;
 using Microsoft.AspNetCore.Mvc;
 using NextERP.ModelBase;
+using NextERP.ModelBase.APIResult;
+using NextERP.ModelBase.PagingResult;
 using NextERP.MVC.Admin.Services.Interfaces;
 using NextERP.MVC.Admin.Services.Services;
 using NextERP.Util;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace NextERP.MVC.Admin.Controllers
 {
@@ -12,12 +17,17 @@ namespace NextERP.MVC.Admin.Controllers
         #region Infrastructure
 
         private readonly IInvoiceAPIService _invoiceAPIService;
+        private readonly IProductAPIService _productAPIService;
+        private readonly IInvoiceDetailAPIService _invoiceDetailAPIService;
         private readonly ISharedCultureLocalizer _localizer;
 
-        public InvoiceController(IInvoiceAPIService invoiceAPIService, IConfiguration configuration, ISharedCultureLocalizer localizer) : base(configuration, localizer)
+        public InvoiceController(IInvoiceAPIService invoiceAPIService, IProductAPIService productAPIService,
+            IConfiguration configuration, ISharedCultureLocalizer localizer, IInvoiceDetailAPIService invoiceDetailAPIService) : base(configuration, localizer)
         {
             _invoiceAPIService = invoiceAPIService;
+            _productAPIService = productAPIService;
             _localizer = localizer;
+            _invoiceDetailAPIService = invoiceDetailAPIService;
         }
 
         #endregion
@@ -124,6 +134,125 @@ namespace NextERP.MVC.Admin.Controllers
         #endregion
 
         #region Custom Operations
+
+        [HttpPost]
+        public async Task<ActionResult> GetProducts(ProductModel request)
+        {
+            FilterModel filter = new FilterModel()
+            {
+                Filters = new List<FilterItemModel>()
+                {
+                    new FilterItemModel()
+                    {
+                        FilterName = Constants.IsDelete,
+                        FilterType = Util.Enums.FilterType.Boolean.ToString(),
+                        FilterOperator = DataHelper.GetBool(request.IsDelete)
+                            ? Util.Enums.FilterOperator.Equal.ToString()
+                            : Util.Enums.FilterOperator.NotEqual.ToString(),
+                    },
+                    new FilterItemModel()
+                    {
+                        FilterName = ProductModel.AttributeNames.ProductCode,
+                        FilterValue = DataHelper.GetString(request.ProductCode),
+                        FilterType = Util.Enums.FilterType.String.ToString(),
+                        FilterOperator = Util.Enums.FilterOperator.Like.ToString(),
+                    },
+                    new FilterItemModel()
+                    {
+                        FilterName = ProductModel.AttributeNames.ExpirationDate,
+                        FilterValue = DataHelper.GetString(DataHelper.GetDateTime(request.ExpirationDate)),
+                        FilterType = Util.Enums.FilterType.Date.ToString(),
+                        FilterOperator = Util.Enums.FilterOperator.Equal.ToString(),
+                    },
+                    new FilterItemModel()
+                    {
+                        FilterName = Constants.DateCreate,
+                        FilterValue = DataHelper.GetString(DataHelper.GetDateTime(request.DateCreate)),
+                        FilterType = Util.Enums.FilterType.Date.ToString(),
+                        FilterOperator = Util.Enums.FilterOperator.Equal.ToString(),
+                    },
+                    new FilterItemModel()
+                    {
+                        FilterName = Constants.DateUpdate,
+                        FilterValue = DataHelper.GetString(DataHelper.GetDateTime(request.DateUpdate)),
+                        FilterType = Util.Enums.FilterType.Date.ToString(),
+                        FilterOperator = Util.Enums.FilterOperator.Equal.ToString(),
+                    },
+                },
+                PageIndex = request.PageIndex,
+                PageSize = request.PageSize,
+            };
+
+            var result = await _productAPIService.GetPaging(filter);
+            if (!DataHelper.ListIsNotNull(result))
+                return Json(_localizer.GetLocalizedString(result.Message));
+
+            foreach (var product in result!.Result!.Items!)
+            {
+                List<string> base64Images = new List<string>();
+
+                foreach (var productImage in product.ProductImages)
+                {
+                    var imageBytes = await _productAPIService.GetImageBytes(DataHelper.GetGuid(productImage.ProductId), DataHelper.GetString(productImage.ImagePath));
+
+                    if (imageBytes.Length > 0)
+                    {
+                        using (var image = Image.Load<Rgba32>(imageBytes, out IImageFormat format))
+                        {
+                            using (var ms = new MemoryStream())
+                            {
+                                image.Save(ms, format); // Lưu lại đúng định dạng gốc
+                                string base64Image = $"data:{format.DefaultMimeType};base64,{Convert.ToBase64String(ms.ToArray())}";
+                                base64Images.Add(base64Image);
+                            }
+                        }
+                    }
+                }
+
+                product.Base64Images = base64Images;
+            }
+
+            return PartialView(ScreenName.Invoice.ProductCardList, result);
+        }
+
+        public async Task<ActionResult> GetInvoiceDetails(InvoiceDetailModel request)
+        {
+            FilterModel filter = new FilterModel()
+            {
+                Filters = new List<FilterItemModel>()
+                {
+                    new FilterItemModel()
+                    {
+                        FilterName = InvoiceDetailModel.AttributeNames.InvoiceDetailCode,
+                        FilterValue = DataHelper.GetString(request.InvoiceDetailCode),
+                        FilterType = Util.Enums.FilterType.String.ToString(),
+                        FilterOperator = Util.Enums.FilterOperator.Like.ToString(),
+                    },
+                    new FilterItemModel()
+                    {
+                        FilterName = Constants.DateCreate,
+                        FilterValue = DataHelper.GetString(DataHelper.GetDateTime(request.DateCreate)),
+                        FilterType = Util.Enums.FilterType.Date.ToString(),
+                        FilterOperator = Util.Enums.FilterOperator.Equal.ToString(),
+                    },
+                    new FilterItemModel()
+                    {
+                        FilterName = Constants.DateUpdate,
+                        FilterValue = DataHelper.GetString(DataHelper.GetDateTime(request.DateUpdate)),
+                        FilterType = Util.Enums.FilterType.Date.ToString(),
+                        FilterOperator = Util.Enums.FilterOperator.Equal.ToString(),
+                    },
+                },
+                PageIndex = request.PageIndex,
+                PageSize = request.PageSize,
+            };
+
+            var result = await _invoiceDetailAPIService.GetPaging(filter);
+            if (!DataHelper.ListIsNotNull(result))
+                return Json(_localizer.GetLocalizedString(result.Message));
+
+            return PartialView(ScreenName.InvoiceDetail.InvoiceDetailList, result);
+        }
 
         #endregion
     }
